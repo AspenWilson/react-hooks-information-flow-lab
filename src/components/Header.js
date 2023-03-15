f3bf6431adc0f07f73c3d5bff79f95f48f3a88fa
Aspen Wilson <aspen.schell@gmail.com>
2023-03-14 17:47:10 -0700

    completed lab

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Header ({onDarkModeClick, darkMode}) {
+
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {darkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+
+export default Header
+
+/*
+Create a Header component by refactoring the <header> element out of the App component. Clicking on the <button> inside of the Header component should still toggle dark mode on and off.
+
+You will need to pass a callback function as a prop called onDarkModeClick to the Header component in order for the test to pass.
+*/
